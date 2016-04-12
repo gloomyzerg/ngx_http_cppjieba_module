49a4a5c2768a8d429b483c96714ee09869cfab1e
gloomyzerg <gloomyzerg@126.com>
2016-04-12 15:11:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# ngx\_http\_cppjieba\_module
+
+## 简介
+
+[CppJieba] 的 `Nginx` 扩展模块。
+
+此模块和[yanyiwu/ngx_http_cppjieba_module](https://github.com/yanyiwu/ngx_http_cppjieba_module)的区别是增加了分词的词性。
+
+此模块是在[CppJieba](https://github.com/yanyiwu/cppjieba) 和 [ngx_http_cppjieba_module](https://github.com/yanyiwu/ngx_http_cppjieba_module) 的基础上完成的。
+
+###在此特别感谢这两个模块的作者 [@yanyiwu](https://github.com/yanyiwu/)
+
+## 用法
+
+
+### 安装和配置
+
+以下用法假设 `ngx_http_cppjieba_module` 下载后存放的地址是 `/tmp/ngx_http_cppjieba_module` (这个地址在 `Nginx` 编译时和词典加载时候会用到)   
+
+#### 下载源码：
+
+```
+git clone git://github.com/gloomyzerg/ngx_http_cppjieba_module.git /tmp/ngx_http_cppjieba_module
+```
+
+#### 进入 `Nginx` 源码目录：
+
+```
+./configure --add-module=/tmp/ngx_http_cppjieba_module/src
+```
+
+因为 `ngx_http_cppjieba_module` 是 `C++` 源码，所以作为 `Nginx` 模块编译的时候需要 修改 `obj/Makefile`
+
+```
+# 1. 在 "CC = gcc" 下面增加一行，如下
+CXX = g++
+# 2. 修改链接器为 g++ ， 如下
+LINK = $(CXX)
+# 3. 修改 ngx_http_cppjieba_module.cpp 的编译器，从 $(CC) 改为 $(CXX) ， 如下
+$(CXX) -c $(CFLAGS)  $(ALL_INCS) \
+    -o objs/addon/src/ngx_http_cppjieba_module.o \
+    /tmp/ngx_http_cppjieba_module/src/ngx_http_cppjieba_module.cpp
+
+```
+
+这三步做完就可以 `make && sudo make install` nginx 了。
+
+```
+# 4. 修改 Nginx 配置文件 /usr/local/nginx/conf/nginx.conf
+location /cppjieba {
+    cppjieba /tmp/ngx_http_cppjieba_module/dict/jieba.dict.utf8 /tmp/ngx_http_cppjieba_module/dict/hmm_model.utf8 /tmp/ngx_http_cppjieba_module/dict/user.dict.utf8;
+}
+```
+
+如果 `ngx_http_cppjieba_module` 的源码路径不是 `/tmp/ngx_http_cppjieba_module`， 将上述过程的 `/tmp/xxx` 改为 `/your/path/xxx` 即可。
+
+## 启动 Nginx
+
+```
+/usr/local/nginx/sbin/nginx
+```
+
+## 测试 
+
+### GET
+
+```
+curl "http://127.0.0.1/cppjieba?s=长春市长春药店"
+```
+
+### POST
+
+```
+curl --data "长春市长春药店" "http://127.0.0.1/cppjieba"
+```
+
+预期结果：
+
+```
+["长春市:ns", "长春:ns", "药店:n"]
+```
+
+用 `chrome` 打开上述链接也可以，不过要 **记得** 设置浏览器的页面编码方式为 `utf-8` 。
---
+++ src/ngx_http_cppjieba_module.cpp
@@ -0,0 +1,337 @@
+extern "C" {
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+}
+
+#include "cppjieba/Jieba.hpp"
+#include "cppjieba/KeywordExtractor.hpp"
+
+using std::string;
+using std::vector;
+using std::pair;
+
+//static ngx_str_t g_cppjieba_conf_arg1;
+//static ngx_str_t g_cppjieba_conf_arg2;
+
+inline unsigned char fromHex(unsigned char x) 
+{
+    return isdigit(x) ? x - '0' : x - 'A' + 10;
+}
+/*
+inline unsigned char toHex(unsigned char x)
+{
+    return x > 9 ? x -10 + 'A': x + '0';
+}
+inline void URLEncode(const string &sIn, string& sOut)
+{
+    for( size_t ix = 0; ix < sIn.size(); ix++ )
+    {      
+        unsigned char buf[4];
+        memset( buf, 0, 4 );
+        if( isalnum( (unsigned char)sIn[ix] ) )
+        {      
+            buf[0] = sIn[ix];
+        }
+        else
+        {
+            buf[0] = '%';
+            buf[1] = toHex( (unsigned char)sIn[ix] >> 4 );
+            buf[2] = toHex( (unsigned char)sIn[ix] % 16);
+        }
+        sOut += (char *)buf;
+    }
+};
+*/
+
+static void URLDecode(const string &sIn, string& sOut)
+{
+    for( size_t ix = 0; ix < sIn.size(); ix++ )
+    {
+        unsigned char ch = 0;
+        if(sIn[ix]=='%')
+        {
+            ch = (fromHex(sIn[ix+1])<<4);
+            ch |= fromHex(sIn[ix+2]);
+            ix += 2;
+        }
+        else if(sIn[ix] == '+')
+        {
+            ch = ' ';
+        }
+        else
+        {
+            ch = sIn[ix];
+        }
+        sOut += (char)ch;
+    }
+}
+cppjieba::Jieba * g_mix_segment;//(DICT_PATH, HMM_PATH, USER_DICT_PATH);
+
+typedef struct {
+    ngx_str_t output_words;
+} ngx_http_cppjieba_loc_conf_t;
+
+// To process HelloWorld command arguments
+static char* ngx_http_cppjieba_set_conf(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
+
+// Allocate memory for HelloWorld command
+static void* ngx_http_cppjieba_create_loc_conf(ngx_conf_t* cf);
+
+// Copy HelloWorld argument to another place
+static char* ngx_http_cppjieba_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child);
+
+//static ngx_int_t ngx_http_cppjieba_init(ngx_cycle_t *cf);
+//static void ngx_http_cppjieba_finalize(ngx_cycle_t *cf);
+
+static ngx_int_t get_post_content(ngx_http_request_t *r, char * data_buf, size_t content_length);
+// Structure for the HelloWorld command
+static ngx_command_t ngx_http_cppjieba_commands[] = {
+    {
+        ngx_string("cppjieba"), // The command name
+        NGX_HTTP_LOC_CONF | NGX_CONF_TAKE3,
+        ngx_http_cppjieba_set_conf, // The command handler
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_http_cppjieba_loc_conf_t, output_words),
+        NULL
+    },
+    ngx_null_command
+};
+
+// Structure for the HelloWorld context
+static ngx_http_module_t ngx_http_cppjieba_module_ctx = {
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    ngx_http_cppjieba_create_loc_conf,
+    ngx_http_cppjieba_merge_loc_conf
+};
+
+// Structure for the HelloWorld module, the most important thing
+ngx_module_t ngx_http_cppjieba_module = {
+    NGX_MODULE_V1,
+    &ngx_http_cppjieba_module_ctx,
+    ngx_http_cppjieba_commands,
+    NGX_HTTP_MODULE,
+    NULL,
+    NULL,
+    NULL, //ngx_http_cppjieba_init,
+    NULL,
+    NULL,
+    NULL, //ngx_http_cppjieba_finalize,
+    NULL,
+    NGX_MODULE_V1_PADDING
+};
+
+static void ngx_http_cppjieba_post_handler(ngx_http_request_t* r);
+
+static ngx_int_t ngx_http_cppjieba_handler(ngx_http_request_t* r) {
+    ngx_int_t rc;
+    ngx_buf_t* b;
+    ngx_chain_t out;
+
+    if(r->method & NGX_HTTP_POST) {
+        ngx_int_t rc = ngx_http_read_client_request_body(r, ngx_http_cppjieba_post_handler);
+        if (rc >= NGX_HTTP_SPECIAL_RESPONSE) {
+            return rc;
+        }
+        return NGX_DONE;
+    }
+
+    if(!(r->method & NGX_HTTP_GET)) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    // args is s=xxxxx
+    ngx_str_t value;
+    if (NGX_OK != ngx_http_arg(r, (u_char*)"s", 1, &value)) {
+        return NGX_HTTP_BAD_REQUEST;
+    }
+
+    string sentence;
+    URLDecode(string((char*)value.data, value.len), sentence);
+    vector<pair<string, string> > tagres;
+    g_mix_segment->Tag(sentence, tagres);
+    string response;
+    //string tmp;
+    //tmp << tagres;
+    //URLEncode(tmp, response);
+    response << tagres;
+
+
+    b = ngx_create_temp_buf(r->pool, response.size());
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    ngx_memcpy(b->pos, response.c_str(), response.size());
+    b->last = b->pos + response.size();
+    b->last_buf = 1;
+
+    out.buf = b;
+    out.next = NULL;
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = response.size();
+    ngx_str_t type = ngx_string("text/plain");
+    r->headers_out.content_type = type;
+
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+
+    return ngx_http_output_filter(r, &out);
+}
+
+static void* ngx_http_cppjieba_create_loc_conf(ngx_conf_t* cf) {
+    ngx_http_cppjieba_loc_conf_t* conf;
+
+    conf = (ngx_http_cppjieba_loc_conf_t*)ngx_pcalloc(cf->pool, sizeof(ngx_http_cppjieba_loc_conf_t));
+    if (conf == NULL) {
+        return NGX_CONF_ERROR;
+    }
+    conf->output_words.len = 0;
+    conf->output_words.data = NULL;
+
+    return conf;
+}
+
+static char* ngx_http_cppjieba_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child) {
+    ngx_http_cppjieba_loc_conf_t* prev = (ngx_http_cppjieba_loc_conf_t*)parent;
+    ngx_http_cppjieba_loc_conf_t* conf = (ngx_http_cppjieba_loc_conf_t*)child;
+    ngx_conf_merge_str_value(conf->output_words, prev->output_words, "Nginx");
+    return NGX_CONF_OK;
+}
+
+static char* ngx_http_cppjieba_set_conf(ngx_conf_t* cf, ngx_command_t* cmd, void* conf) {
+    ngx_http_core_loc_conf_t* clcf;
+    clcf = (ngx_http_core_loc_conf_t*)ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_cppjieba_handler;
+    ngx_conf_set_str_slot(cf, cmd, conf);
+    if (cf->args->nelts != 4) {
+        ngx_log_error(NGX_LOG_ERR, cf->log, 0, " [the number of conf'a args is not 4] ");
+        return (char*)NGX_CONF_ERROR;
+    }
+    ngx_str_t * value = (ngx_str_t *)cf->args->elts;
+
+    g_mix_segment = new cppjieba::Jieba(
+                string((const char *)value[1].data, value[1].len), 
+                string((const char *)value[2].data, value[2].len),
+                string((const char *)value[3].data, value[3].len));
+    return NGX_CONF_OK;
+}
+
+//static ngx_int_t ngx_http_cppjieba_init(ngx_cycle_t *cf) 
+//{
+//    g_mix_segment = new cppjieba::Jieba(
+//                string((const char *)g_cppjieba_conf_arg1.data, g_cppjieba_conf_arg1.len), 
+//                string((const char *)g_cppjieba_conf_arg2.data, g_cppjieba_conf_arg2.len));
+//    return NGX_OK;
+//}
+
+//static void ngx_http_cppjieba_finalize(ngx_cycle_t *cf)
+//{
+//    delete g_mix_segment;
+//    g_mix_segment = NULL;
+//}
+
+static ngx_int_t get_post_content(ngx_http_request_t *r, char * data_buf, size_t content_length) {
+    ngx_log_error(NGX_LOG_NOTICE, r->connection->log, 0, "[get_post_content] [content_length:%d]", content_length); //DEBUG
+    if(r->request_body == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "reqeust_body:null");
+        return NGX_ERROR;
+    }
+    ngx_chain_t* bufs = r->request_body->bufs;
+    ngx_buf_t* buf = NULL;
+    size_t body_length = 0;
+    size_t buf_length;
+    while(bufs) {
+        buf = bufs->buf;
+        bufs = bufs->next;
+        buf_length = buf->last - buf->pos;
+        if(body_length + buf_length > content_length) {
+            memcpy(data_buf + body_length, buf->pos, content_length - body_length);
+            body_length = content_length;
+            break;
+        }
+        memcpy(data_buf + body_length, buf->pos, buf->last - buf->pos);
+        body_length += buf->last - buf->pos;
+    }
+    if(body_length != content_length) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "get_post_content's body_length != content_length in headers");
+        return NGX_ERROR;
+    }
+    return NGX_OK;
+}
+
+
+static ngx_int_t ngx_http_cppjieba_send_response(ngx_http_request_t * r, const char* type, const char* data_buf, size_t len) {
+    ngx_int_t rc;
+    ngx_buf_t* b;
+    ngx_chain_t out;
+
+    b = ngx_create_temp_buf(r->pool, len);
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    ngx_memcpy(b->pos, data_buf, len);
+    b->last = b->pos + len;
+    b->last_buf = 1;
+
+    out.buf = b;
+    out.next = NULL;
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = len;
+    r->headers_out.content_type.data = (u_char*) type;
+    r->headers_out.content_type.len = strlen(type);
+
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+
+    return ngx_http_output_filter(r, &out);
+}
+
+static void ngx_http_cppjieba_post_handler(ngx_http_request_t* r) {
+    if(r->headers_in.content_length_n == 0) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "r->headers_in.content_length_n is 0");
+        ngx_http_finalize_request(r, NGX_ERROR);
+        return;
+    }
+    ngx_int_t rc;
+    char * data_buf = NULL;
+    data_buf = (char*) ngx_pcalloc(r->pool, r->headers_in.content_length_n + 1);
+    if (data_buf == NULL) {
+        ngx_http_finalize_request(r, NGX_ERROR);
+        return;
+    }
+
+    if (NGX_ERROR == get_post_content(r, data_buf, r->headers_in.content_length_n)) {
+        ngx_http_finalize_request(r, NGX_ERROR);
+        return;
+    }
+
+    string sentence;
+    URLDecode(data_buf, sentence);
+    vector<pair<string, string> > tagres;
+    g_mix_segment->Tag(sentence, tagres);
+    string response;
+    response << tagres;
+
+
+    rc = ngx_http_cppjieba_send_response(
+                r, 
+                "text/plain", 
+                response.c_str(), 
+                response.size());
+
+    ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "[ngx_http_cppjieba_send_response] [response size:%d]", response.size());
+    ngx_http_finalize_request(r, rc);
+}
